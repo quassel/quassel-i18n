cd2dfa79aaffd15efce98ba74100dc8a8d4a08e9
Daniel Albers <daniel@lbe.rs>
2016-03-06 22:22:55 +0100

    Call lupdate with -codecfortr utf-8.

--- po/update-pot.sh
+++ po/update-pot.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-lupdate ../src -ts quassel.ts && lconvert -i quassel.ts -o quassel.po        \
+lupdate ../src -codecfortr utf-8 -ts quassel.ts                              \
+  && lconvert -i quassel.ts -o quassel.po                                    \
   && msguniq -o quassel.pot quassel.po && rm quassel.ts quassel.po           \
   && patch -Np2 < quassel.pot.patch                                          \
   && sed -i -re 's/^msgstr\[0\] ""/msgstr[0] ""\nmsgstr[1] ""/;' quassel.pot
